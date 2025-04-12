c030cdbcde548a41b22114aa4a4b09a47c105c4c
Une Nyide <une.nyide@gmail.com>
2025-04-12 22:50:09 +0200

    chore: reset theme and add day one component

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,10 @@
 import "./App.css";
+import DayOne from "./components/day-one";
 
 function App() {
   return (
     <main>
-      <section className="prac-1">
-        <h3>Day 1</h3>
-        <h5>Christmas Ornament</h5>
-        <svg width={200} height={200}>
-          <circle cx={100} cy={120} r={70} fill="#D1495B" />
-          <rect x={82} y={35} height={20} width={36} fill="#f79257" />
-          <circle
-            cx={100}
-            cy={25}
-            r={12}
-            fill="none"
-            stroke="#F79257"
-            strokeWidth={2}
-          />
-        </svg>
-      </section>
+      <DayOne />
     </main>
   );
 }
