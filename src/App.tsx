053ab48261c626a37751310e37714405557abd2c
Une Nyide <une.nyide@gmail.com>
2025-04-12 21:52:26 +0200

    feat: add first practical

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,22 @@ import "./App.css";
 function App() {
   return (
     <main>
-      <h1>Svgs</h1>
+      <section className="prac-1">
+        <h3>Day 1</h3>
+        <h5>Christmas Ornament</h5>
+        <svg width={200} height={200}>
+          <circle cx={100} cy={120} r={70} fill="#D1495B" />
+          <rect x={82} y={35} height={20} width={36} fill="#f79257" />
+          <circle
+            cx={100}
+            cy={25}
+            r={12}
+            fill="none"
+            stroke="#F79257"
+            strokeWidth={2}
+          />
+        </svg>
+      </section>
     </main>
   );
 }
