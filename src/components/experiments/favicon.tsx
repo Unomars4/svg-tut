db02a6993ca403415665211ec413de82f2343665
Une Nyide <une.nyide@gmail.com>
2025-04-19 09:20:29 +0200

    feat: add favicon logo

--- src/components/experiments/favicon.tsx
+++ src/components/experiments/favicon.tsx
@@ -6,11 +6,11 @@ export default function Favicon() {
     <PracticalLayout day={100} title="Favicon">
       <SVGLayout>
         <defs>
-          <linearGradient id="logoColors">
-            <stop offset="30%" stop-color="#fb4934" />
-            <stop offset="43%" stop-color="#98971a" />
+          <radialGradient id="logoColors">
+            <stop offset="30%" stop-color=" #9d0006" />
+            <stop offset="40%" stop-color="#98971a" />
             <stop offset="85%" stop-color="#076678" />
-          </linearGradient>
+          </radialGradient>
         </defs>
         <circle fill="url('#logoColors')" cx={0} cy={0} r={50} />
       </SVGLayout>
