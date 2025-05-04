d65737ee5407edc48c5f46017f1d79ebe0dda4ab
Une Nyide <une.nyide@gmail.com>
2025-05-04 13:27:30 +0200

    chore: change logo color

--- src/components/experiments/favicon.tsx
+++ src/components/experiments/favicon.tsx
@@ -5,14 +5,7 @@ export default function Favicon() {
   return (
     <PracticalLayout day={100} title="Favicon">
       <SVGLayout>
-        <defs>
-          <radialGradient id="logoColors">
-            <stop offset="30%" stop-color=" #9d0006" />
-            <stop offset="40%" stop-color="#98971a" />
-            <stop offset="85%" stop-color="#076678" />
-          </radialGradient>
-        </defs>
-        <circle fill="url('#logoColors')" cx={0} cy={0} r={50} />
+        <circle fill="#d79921" cx={0} cy={0} r={50} />
       </SVGLayout>
     </PracticalLayout>
   );
